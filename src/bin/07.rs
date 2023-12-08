d440f028d47b2bb6034714fba7ab8a0f4b5ee47e
Matt Clarke <Matt.Clarke@wae.com>
2023-12-08 00:59:38 +0000

    cargo fmt

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,13 +1,31 @@
 advent_of_code::solution!(7);
 
-use std::str::FromStr;
 use counter::Counter;
+use std::str::FromStr;
 
 #[derive(Debug)]
-enum Errors {ParsingError, CardCountError}
+enum Errors {
+    ParsingError,
+    CardCountError,
+}
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Hash, Clone, Copy)]
-enum Card {WildJack, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+enum Card {
+    WildJack,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
 
 impl Card {
     fn from_char(c: char) -> Result<Card, Errors> {
@@ -26,7 +44,7 @@ impl Card {
             '3' => Ok(Card::Three),
             '2' => Ok(Card::Two),
             '1' => Ok(Card::WildJack),
-            _ => Err(Errors::ParsingError)
+            _ => Err(Errors::ParsingError),
         }
     }
 }
@@ -45,80 +63,131 @@ enum Rank {
 #[derive(Debug, Ord, PartialEq, PartialOrd, Eq)]
 struct Hand {
     rank: Rank,
-    bid: usize
+    bid: usize,
 }
 
 impl FromStr for Hand {
     type Err = Errors;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-
         let (card_c, bid) = s.split_once(' ').unwrap();
-        let cards:Vec<Card> = card_c.chars().map(|f| Card::from_char(f).unwrap()).collect();
+        let cards: Vec<Card> = card_c
+            .chars()
+            .map(|f| Card::from_char(f).unwrap())
+            .collect();
         let counts: Counter<&Card> = cards.iter().collect::<Counter<_>>();
 
-        let rank: Result<Rank, Errors>= match counts.most_common_ordered()[..] {
-            [(_, 1), (_, 1), (_, 1), (_, 1), (_, 1)] => Ok(Rank::HighCard(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 2), (_, 1), (_, 1), (_, 1)] => Ok(Rank::Pair(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 2), (_, 2), (_, 1)] => Ok(Rank::TwoPair(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 3), (_, 1), (_, 1)] => Ok(Rank::ThreeOfAKind(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 3), (_, 2)] => Ok(Rank::FullHouse(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 4), (_, 1)] => Ok(Rank::FourOfAKind(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 5)] => Ok(Rank::FiveOfAKind(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            _ => Err(Errors::CardCountError)
+        let rank: Result<Rank, Errors> = match counts.most_common_ordered()[..] {
+            [(_, 1), (_, 1), (_, 1), (_, 1), (_, 1)] => Ok(Rank::HighCard(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, 2), (_, 1), (_, 1), (_, 1)] => {
+                Ok(Rank::Pair(cards[0], cards[1], cards[2], cards[3], cards[4]))
+            }
+            [(_, 2), (_, 2), (_, 1)] => Ok(Rank::TwoPair(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, 3), (_, 1), (_, 1)] => Ok(Rank::ThreeOfAKind(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, 3), (_, 2)] => Ok(Rank::FullHouse(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, 4), (_, 1)] => Ok(Rank::FourOfAKind(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, 5)] => Ok(Rank::FiveOfAKind(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            _ => Err(Errors::CardCountError),
         };
 
-
-        Ok(Hand{rank: rank.unwrap(), bid: bid.parse().unwrap()})
-    }
-}
-impl Hand{
-    fn from_str_wild(s: &str) -> Result<Self, Errors> {
-
-        let (card_c, bid) = s.split_once(' ').unwrap();
-        let cards:Vec<Card> = card_c.chars()
-        .map(|f| match f {
-            'J' => '1',
-            _ => f
+        Ok(Hand {
+            rank: rank.unwrap(),
+            bid: bid.parse().unwrap(),
         })
-        .map(|f| Card::from_char(f).unwrap()).collect();
-
-        let counts: Counter<&Card> = cards.iter().filter(|f| **f != Card::WildJack).collect::<Counter<_>>();
-        let rank: Result<Rank, Errors>= match counts.most_common_ordered()[..] {
-
-            [] | [(_, _)] => Ok(Rank::FiveOfAKind(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, _), (_, 1)] => Ok(Rank::FourOfAKind(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, _), (_, 2)] => Ok(Rank::FullHouse(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, _), (_, 1), (_, 1)] => Ok(Rank::ThreeOfAKind(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 2), (_, 2), (_, 1)] => Ok(Rank::TwoPair(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 2), (_, 1), (_, 1), (_, 1)] | [(_, 1), (_, 1), (_, 1), (_, 1)] => Ok(Rank::Pair(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            [(_, 1), (_, 1), (_, 1), (_, 1), (_, 1)] => Ok(Rank::HighCard(cards[0], cards[1], cards[2], cards[3], cards[4])),
-            _ => Err(Errors::CardCountError)
-        };
-
-
-        Ok(Hand{rank: rank.unwrap(), bid: bid.parse().unwrap()})
     }
 }
+impl Hand {
+    fn from_str_wild(s: &str) -> Result<Self, Errors> {
+        let (card_c, bid) = s.split_once(' ').unwrap();
+        let cards: Vec<Card> = card_c
+            .chars()
+            .map(|f| match f {
+                'J' => '1',
+                _ => f,
+            })
+            .map(|f| Card::from_char(f).unwrap())
+            .collect();
 
+        let counts: Counter<&Card> = cards
+            .iter()
+            .filter(|f| **f != Card::WildJack)
+            .collect::<Counter<_>>();
+        let rank: Result<Rank, Errors> = match counts.most_common_ordered()[..] {
+            [] | [(_, _)] => Ok(Rank::FiveOfAKind(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, _), (_, 1)] => Ok(Rank::FourOfAKind(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, _), (_, 2)] => Ok(Rank::FullHouse(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, _), (_, 1), (_, 1)] => Ok(Rank::ThreeOfAKind(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, 2), (_, 2), (_, 1)] => Ok(Rank::TwoPair(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            [(_, 2), (_, 1), (_, 1), (_, 1)] | [(_, 1), (_, 1), (_, 1), (_, 1)] => {
+                Ok(Rank::Pair(cards[0], cards[1], cards[2], cards[3], cards[4]))
+            }
+            [(_, 1), (_, 1), (_, 1), (_, 1), (_, 1)] => Ok(Rank::HighCard(
+                cards[0], cards[1], cards[2], cards[3], cards[4],
+            )),
+            _ => Err(Errors::CardCountError),
+        };
 
+        Ok(Hand {
+            rank: rank.unwrap(),
+            bid: bid.parse().unwrap(),
+        })
+    }
+}
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut cards: Vec<Hand> = input.split('\n').filter(|f| !f.is_empty()).map(|f| Hand::from_str(f).unwrap()).collect();
-    
+    let mut cards: Vec<Hand> = input
+        .split('\n')
+        .filter(|f| !f.is_empty())
+        .map(|f| Hand::from_str(f).unwrap())
+        .collect();
+
     cards.sort();
 
-    let s: usize = cards.iter().enumerate().map(|(index, value)| (value.bid * (index + 1))).sum();
+    let s: usize = cards
+        .iter()
+        .enumerate()
+        .map(|(index, value)| (value.bid * (index + 1)))
+        .sum();
 
     Some(s)
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let mut cards: Vec<Hand> = input.split('\n').filter(|f| !f.is_empty()).map(|f| Hand::from_str_wild(f).unwrap()).collect();
-    
+    let mut cards: Vec<Hand> = input
+        .split('\n')
+        .filter(|f| !f.is_empty())
+        .map(|f| Hand::from_str_wild(f).unwrap())
+        .collect();
+
     cards.sort();
 
-    let s: usize = cards.iter().enumerate().map(|(index, value)| (value.bid * (index + 1))).sum();
+    let s: usize = cards
+        .iter()
+        .enumerate()
+        .map(|(index, value)| (value.bid * (index + 1)))
+        .sum();
 
     Some(s)
 }
@@ -131,18 +200,44 @@ mod tests {
     fn test_parse() {
         let input = &advent_of_code::template::read_file("examples", DAY);
 
-        let cards: Vec<Hand> = input.split('\n').filter(|f| f.len() > 0).map(|f| Hand::from_str(f).unwrap()).collect();
+        let cards: Vec<Hand> = input
+            .split('\n')
+            .filter(|f| f.len() > 0)
+            .map(|f| Hand::from_str(f).unwrap())
+            .collect();
 
-        assert_eq!(cards[0], Hand{rank: Rank::HighCard(Card::Two, Card::Three, Card::Four, Card::Five, Card::Ace), bid: 1});
+        assert_eq!(
+            cards[0],
+            Hand {
+                rank: Rank::HighCard(Card::Two, Card::Three, Card::Four, Card::Five, Card::Ace),
+                bid: 1
+            }
+        );
     }
 
     #[test]
     fn test_parse_wild() {
         let input = &advent_of_code::template::read_file("examples", DAY);
 
-        let cards: Vec<Hand> = input.split('\n').filter(|f| f.len() > 0).map(|f| Hand::from_str_wild(f).unwrap()).collect();
+        let cards: Vec<Hand> = input
+            .split('\n')
+            .filter(|f| f.len() > 0)
+            .map(|f| Hand::from_str_wild(f).unwrap())
+            .collect();
 
-        assert_eq!(cards[1], Hand{rank: Rank::ThreeOfAKind(Card::Queen, Card::Two, Card::King, Card::WildJack, Card::WildJack), bid: 13});
+        assert_eq!(
+            cards[1],
+            Hand {
+                rank: Rank::ThreeOfAKind(
+                    Card::Queen,
+                    Card::Two,
+                    Card::King,
+                    Card::WildJack,
+                    Card::WildJack
+                ),
+                bid: 13
+            }
+        );
     }
 
     #[test]
